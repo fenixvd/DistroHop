cd643bcc81c04efb0cbe153b3b4d1f9eea10c632
fenixvd <mrfenixvd@yandex.ru>
2023-05-11 09:12:55 +0300

    first test

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Определение дистрибутива и установка софта
+if [ -e /etc/debian_version ]; then
+  # Установка софта для Debian/Ubuntu/Mint
+  if [ -e /usr/bin/apt-get ]; then
+    sudo apt-get install zsh mc wget curl telnet nano neofetch -y
+  fi
+elif [ -e /etc/redhat-release ]; then
+  # Установка софта для CentOS/Oracle Linux/Red Hat
+  if [ -e /usr/bin/yum ]; then
+    sudo yum install zsh mc wget curl telnet nano neofetch -y
+    fi
+  # Установка софта для Fedora
+  if [ -e /usr/bin/dnf ]; then
+    sudo dnf install zsh mc wget curl telnet nano neofetch  -y
+  fi
+  # Установка софта для OpenSUSE
+  if [ -e /usr/bin/zypper ]; then
+    sudo zypper install zsh mc wget curl telnet neofetch -y
+  fi
+fi
+
+# Установка английской локали
+sudo sed -i 's/^#\s*\(en_US.UTF-8\)/\1/' /etc/locale.gen
+sudo locale-gen
+
+# Установка ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" -unattended
+
+# Скачивание и замена файла .zshrc
+curl -o ~/.zshrc https://nextcloud.2admins.ru/index.php/s/yocxf9anHaxYAK6
+
+# Настройка zsh
+source ~/.zshrc
+
+# Установка zsh оболочкой по умолчанию
+if [ -e /bin/zsh ]; then
+  chsh -s /bin/zsh
+fi
+
+echo "Софт установлен, а так же установлен zsh как стандартный шелл."
